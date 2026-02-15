c7bbe5cba9756303042b2bc44cfcfc69b82dcdf2
Cristiam Vasques <crvasques@outlook.com>
2026-02-15 01:14:39 +0000

    Atual

--- Introdução ao Processamento da Informação/03_Semana/CaractereNaPosicao.py
+++ Introdução ao Processamento da Informação/03_Semana/CaractereNaPosicao.py
@@ -1,15 +1,23 @@
 """"Receba uma string e um inteiro e retorne o caractere que está na posição da string correspondente 
 ao inteiro informado."""
 
-entrada = input()
-       
-# Divide a string pelo espaço
-partes = entrada.split()
+while True:
+    try:
+        # Lê a linha inteira da entrada
+        entrada = input()
         
-# Verifica se temos os dois componentes: S e P
-if len(partes) == 2:
-    s = partes[0]      # A string S 
-    p = int(partes[1]) # A posição P 
+        # Divide a string pelo espaço
+        partes = entrada.split()
+        
+        # Verifica se temos os dois componentes: S e P
+        if len(partes) == 2:
+            s = partes[0]      # A string S 
+            p = int(partes[1]) # A posição P 
             
-# Imprime o caractere na posição P e salta uma linha 
-print(s[p])
+            # Imprime o caractere na posição P e salta uma linha 
+            print(s[p])
+            
+    except EOFError:
+        # Quando o input() atinge o fim do arquivo, ele gera esta exceção
+        break
+    
