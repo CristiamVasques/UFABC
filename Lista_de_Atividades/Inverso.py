1255e2fc015c1c421a5b9c5ec5c3a414bb2a6ede
Cristiam Vasques <crvasques@outlook.com>
2026-02-15 04:02:53 +0000

    Atual

---
+++ Lista_de_Atividades/Inverso.py
@@ -0,0 +1,8 @@
+while True:
+    try:
+        str = input();
+        for i in range(len(str)-1, -1, -1):
+            print(str[i], end="");
+        print();
+    except EOFError:
+        break;
